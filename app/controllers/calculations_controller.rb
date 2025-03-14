021bdbb14cbd0a69207ae4c2f607a1dc53959a68
tomball357 <tball@chicagobooth.edu>
2025-03-14 22:33:22 +0000

    set

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,52 @@
+class CalculationsController < ApplicationController
+  def square_form
+    render "square_form"
+  end
+
+  def square_results
+    @number = params[:number].to_f
+    @result = @number**2
+    render "square_results"
+  end
+
+  def square_root_form
+    render "square_root_form"
+  end
+
+  def square_root_results
+    @num = params[:number].to_f
+    @result = Math.sqrt(@num)
+    render "square_root_results"
+  end
+
+  def payment_form
+    render "payment_form"
+  end
+
+  def payment_results
+    @apr = params[:apr].to_f / 100 / 12
+    @years = params[:years].to_f * 12
+    @principal = params[:principal].to_f
+
+    if @apr > 0
+      @monthly_payment = (@apr * @principal) / (1 - (1 + @apr) ** -@years)
+    else
+      @monthly_payment = @principal / @years
+    end
+
+    render "payment_results"
+  end
+
+  def random_form
+    render "random_form"
+  end
+
+  def random_results
+    @min = params[:min].to_f
+    @max = params[:max].to_f
+
+    @result = rand(@min..@max).round(16)
+    
+    render "random_results"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  
+  root "calculations#square_form"
+
+  get "/square/new", to: "calculations#square_form"
+  get "/square/results", to: "calculations#square_results"
+
+  get "/square_root/new", to: "calculations#square_root_form"
+  get "/square_root/results", to: "calculations#square_root_results"
+
+  get "/payment/new", to: "calculations#payment_form"
+  get "/payment/results", to: "calculations#payment_results"
+
+  get "/random/new", to: "calculations#random_form"
+  get "/random/results", to: "calculations#random_results"
 end
